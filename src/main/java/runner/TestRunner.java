da45395ba95e6cac9102b8b94cc22a5e386e5fa8
Yogesh Rana <36674300+yrana17@users.noreply.github.com>
2024-02-22 15:49:05 +0530

    Create TestRunner.java

---
+++ src/main/java/runner/TestRunner.java
@@ -0,0 +1,30 @@
+package runner;
+
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class TestRunner {
+	
+	
+	@SuppressWarnings("deprecation")
+	@Test
+	public void test()
+	{
+		System.out.println("Testing the Selenium Application");
+		
+		WebDriver driver;
+		ChromeOptions options = new ChromeOptions();
+		options.setAcceptInsecureCerts(true);
+		options.setHeadless(true);
+		driver = new ChromeDriver(options);
+		System.out.println("Start Testing");
+		driver.get("https://www.google.com");
+		System.out.println(driver.getTitle());
+		System.out.println("Ending Testing");
+		driver.close();
+	}
+	
+
+}
